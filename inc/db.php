565aa24f119ec8abf33072e5dd6348f3c85b618f
Chiara Ali <ali.chiara.uni@gmail.com>
2025-06-24 12:35:30 +0200

    Adds database connection and post retrieval functionality
    Introduces a database connection using MySQLi in the new db.php file and retrieves posts from the database in index.php.
    
    Removes outdated configuration file.

--- inc/db.php
+++ inc/db.php
@@ -1,1 +1,20 @@
+ <?php 
  
+// Connect to the database trough phpMyAdmin
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "blog_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+
+if($conn ->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+}
+
+
+?>
--- index.php
+++ index.php
@@ -1,1 +1,23 @@
- 
+<?php 
+
+require_once 'inc/db.php';
+
+$sql = "SELECT * FROM posts";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<h2>" . $row["title"] . "</h2>";
+        echo "<p>" . $row["content"] . "</p>";
+        echo "<hr>";
+    }
+} else {
+    echo "Nessun post trovato";
+}
+
+
+$conn->close();
+
+
+?>
